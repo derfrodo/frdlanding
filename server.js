c7728a7f7c915d72b4e8e419b6c59c51f5d21b00
derfrodo <derfrodo0715@gmail.com>
2017-03-18 14:21:58 +0100

    port geaendert

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ var app = express();
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express app is listening on port 3000. Visit it at http://localhost:3000 ");
 });
